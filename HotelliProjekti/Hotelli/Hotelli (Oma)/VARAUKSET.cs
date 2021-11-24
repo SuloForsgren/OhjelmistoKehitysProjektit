37c16a4057a5a09c1344f530ac0ae887212db36d
SuloForsgren <sulo.forsgren@gmail.com>
2021-11-24 11:32:20 +0200

    HotelliProjektin alku

---
+++ HotelliProjekti/Hotelli/Hotelli (Oma)/VARAUKSET.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hotelli__Oma_
+{
+    public partial class VARAUKSET : Form
+    {
+        public VARAUKSET()
+        {
+            InitializeComponent();
+        }
+
+        private void VARAUKSET_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
